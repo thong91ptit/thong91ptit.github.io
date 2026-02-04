2ee4c61ffe8ea0e11f94f719d557941bf02591a7
mtgamestudio <thong91ptit@gmail.com>
2026-02-04 16:06:08 +0700

    init

--- src/app/components/LoadingScreen.tsx
+++
@@ -1,7 +0,0 @@
-export function LoadingScreen() {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-white border-t-transparent" />
-    </div>
-  );
-}
